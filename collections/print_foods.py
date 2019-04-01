daf8c648e786303347441851c60436ddade0a270
FX-Wood <fxwood2@gmail.com>
2019-04-01 14:58:25 -0700

    Done!

---
+++ collections/print_foods.py
@@ -0,0 +1,8 @@
+# Exercise 2
+# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+# Use a for loop to print out the string "food goes here is a good food"
+
+foods = ('jelly', 'pesto', 'tapenade', 'cheese')
+
+for food in foods:
+    print(f"{food} is a good food")
